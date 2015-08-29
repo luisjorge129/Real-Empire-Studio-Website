f62f0e21940ec1b9596ffebd6e472137e9780aa9
Luis J. Perez <luisjorge129@gmail.com>
2015-08-29 16:11:31 -0400

    Added more pages.

--- landing_page/urls.py
+++ landing_page/urls.py
@@ -1,6 +1,12 @@
 from django.conf.urls import url
-from .views import LandingPage
+from .views import LandingView
+from .views import PriceView
+from .views import ClassView
+from .views import ContactView
 
 urlpatterns = [
-    url(r'^$', LandingPage.as_view(), name='landing'),
+    url(r'^$', LandingView.as_view(), name='landing'),
+    url(r'^precios/$', PriceView.as_view(), name='price'),
+    url(r'^clases/$', ClassView.as_view(), name='class'),
+    url(r'^contacto/$', ContactView.as_view(), name='contact'),
 ]
