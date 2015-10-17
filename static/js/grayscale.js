8092628e883eef9b52ddeb85559f669d1ed86b10
Luis J. Perez <luisjorge129@gmail.com>
2015-10-17 00:03:59 -0400

    Changes to images and SEO

--- static/js/grayscale.js
+++ static/js/grayscale.js
@@ -17,3 +17,39 @@ $(function() {
         event.preventDefault();
     });
 });
+
+
+$(".navbar-toggle").click(function(){
+    if($(".collapse").css("display") === "none"){
+        $(".collapse").slideDown();
+    }else
+        $(".collapse").slideUp();
+});
+
+$(function () {
+  var nua = navigator.userAgent
+  var isAndroid = (nua.indexOf('Mozilla/5.0') > -1 && nua.indexOf('Android ') > -1 && nua.indexOf('AppleWebKit') > -1 && nua.indexOf('Chrome') === -1)
+  if (isAndroid) {
+    $('select.form-control').removeClass('form-control').css('width', '100%')
+  }
+}) 
+
+// Copyright 2014-2015 Twitter, Inc.
+// Licensed under MIT (https://github.com/twbs/bootstrap/blob/master/LICENSE)
+if (navigator.userAgent.match(/IEMobile\/10\.0/)) {
+  var msViewportStyle = document.createElement('style')
+  msViewportStyle.appendChild(
+    document.createTextNode(
+      '@-ms-viewport{width:auto!important}'
+    )
+  )
+  document.querySelector('head').appendChild(msViewportStyle)
+}
+
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+      ga('create', 'UA-49969090-3', 'auto');
+      ga('send', 'pageview');
