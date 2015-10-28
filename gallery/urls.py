a4ea75a0f800b4b9e5cc607b6353c40ed860c381
Luis J. Perez <luisjorge129@gmail.com>
2015-10-27 20:16:27 -0400

    Changes

--- gallery/urls.py
+++ gallery/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 
 urlpatterns = [
-    url(r'^video/$', views.VideoListView.as_view(),
+    url(r'^videos/$', views.VideoListView.as_view(),
     	name='video_list'),
 	url(r'^fotos/$', views.GalleryListView.as_view(),
     	name='image_list'),
