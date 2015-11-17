cfcf1a53b71f4c739b67e7bb5c4c1a53b1e66786
Luis J. Perez <luisjorge129@gmail.com>
2015-11-17 01:04:50 -0400

    Add

--- real_empire_studio/settings/prod.py
+++ real_empire_studio/settings/prod.py
@@ -33,3 +33,10 @@ def get_env_setting(setting):
         raise ImproperlyConfigured(error_msg)
 
 SECRET_KEY = get_env_setting('SECRET_KEY')
+
+AWS_STORAGE_BUCKET_NAME = os.environ['AWS_STORAGE_BUCKET_NAME']
+AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
+AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
+STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+S3_URL = 'http://%s.s3.amazonaws.com/' % AWS_STORAGE_BUCKET_NAME
+STATIC_URL = S3_URL
