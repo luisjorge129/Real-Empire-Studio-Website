725610f76005d2e0271ca9cc56d52df4ebb0c557
Luis J. Perez <luisjorge129@gmail.com>
2016-01-09 12:21:40 -0400

    Changes

--- real_empire_studio/settings/prod.py
+++ real_empire_studio/settings/prod.py
@@ -51,7 +51,6 @@ def get_env_setting(setting):
 
 SECRET_KEY = get_env_setting('SECRET_KEY')
 
-
 AWS_STORAGE_BUCKET_NAME = os.environ['AWS_STORAGE_BUCKET_NAME']
 AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
 AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
