7029301bafcb5685f8eb7816c9dab9066eb5e353
Luis J. Perez <luisjorge129@gmail.com>
2016-01-16 02:13:07 -0400

    Changes

--- events/models.py
+++ events/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 from autoslug import AutoSlugField
+from redactor.fields import RedactorField
 
 from core.models import TimeStampedModel
 from core.models import Image
@@ -8,7 +9,8 @@ from core.models import Image
 
 class Event(TimeStampedModel, Image):
     name = models.CharField(max_length=100)
-    description = models.TextField(blank=True)
+    description = RedactorField(verbose_name=u'Text',
+                              blank=True)
     slug = AutoSlugField(populate_from='name',
                          unique=True, max_length=50)
     time = models.DateTimeField()
