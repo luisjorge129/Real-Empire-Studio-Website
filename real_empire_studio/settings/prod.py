bd3aee4deef08bc2a5940971b36788dab00214f1
Luis J. Perez <luisjorge129@gmail.com>
2016-01-19 23:57:24 -0400

    small changes

--- real_empire_studio/settings/prod.py
+++ real_empire_studio/settings/prod.py
@@ -56,7 +56,7 @@ AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
 AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
-STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+# STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
 
 S3_URL = 'http://%s.s3.amazonaws.com/' % AWS_STORAGE_BUCKET_NAME
 MEDIA_DIRECTORY = '/media/'
